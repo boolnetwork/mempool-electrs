673ac20bdc0913d901ce5f5b2de9bc8a4e586343
Sato Kenta <hannirock123@gmail.com>
2024-10-12 12:38:31 +0800

    fix

--- src/bin/tx-fingerprint-stats.rs
+++ src/bin/tx-fingerprint-stats.rs
@@ -39,7 +39,7 @@ fn main() {
             config.magic,
             signal,
             &metrics,
-            config.spv_url,
+            config.spv_url.clone(),
         )
             .unwrap(),
     );
